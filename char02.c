004c3ce7c8baaa578b92c4af440f588b50f5fe71
Isha Negi <itsishanegi@gmail.com>
2024-09-16 20:08:25 +0530

    Add files via upload

---
+++ char02.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+
+{ char a[2][4]={'a','d','f','g','t','y','p','j'};
+  
+for(int i=0;i<2;i++)
+  
+{ for(int j=0;j<4;j++)
+  
+{  printf("(%d,%d)= %c\n",i,j,a[i][j]);
+  
+}
+  
+}
+
+}
